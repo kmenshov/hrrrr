f05f2095eb5ffe215506911fae8c5e0de5e7fa89
k.menshov <k.menshov@gmail.com>
2019-03-16 17:15:09 +0300

    Added Good

--- server/manifest.js
+++ server/manifest.js
@@ -3,6 +3,32 @@
 const Confidence = require('confidence');
 const Path = require('path');
 
+const goodOptions = {
+  ops: {
+    $filter: { $env: 'NODE_ENV' },
+    $default: { interval: 1 * 60 * 1000 },
+    production: { interval: 5 * 60 * 1000 },
+    test: false,
+  },
+  reporters: {
+    consoleReporter: [
+      {
+        module: 'good-squeeze',
+        name: 'Squeeze',
+        args: [{
+          $filter: { $env: 'NODE_ENV' },
+          $default: { log: '*', request: '*', response: '*', ops: '*' },
+          test: {},
+        }],
+      },
+      {
+        module: 'good-console',
+      },
+      'stdout',
+    ],
+  },
+};
+
 // Glue manifest as a confidence store
 module.exports = new Confidence.Store({
   server: {
@@ -17,19 +43,20 @@ module.exports = new Confidence.Store({
         relativeTo: Path.resolve(__dirname, '../public'),
       },
     },
-    debug: {
-      $filter: { $env: 'NODE_ENV' },
-      $default: {
-        log: ['error'],
-        request: ['error'],
-      },
-      production: {
-        request: ['implementation'],
-      },
-    },
+    // debug: {
+    //   $filter: { $env: 'NODE_ENV' },
+    //   $default: {
+    //     log: ['error'],
+    //     request: ['error'],
+    //   },
+    //   production: {
+    //     request: ['implementation'],
+    //   },
+    // },
   },
   register: {
     plugins: [
+      { plugin: 'good', options: goodOptions },
       { plugin: 'inert' },
       { plugin: `${__dirname}/web` },
       { plugin: `${__dirname}/api`, routes: { prefix: '/api/v0' } },
